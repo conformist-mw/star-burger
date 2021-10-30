d6c6b95ce532cca3042357c57948934c772c92c5
Oleg Smedyuk <conformist.mw@gmail.com>
2021-10-30 23:34:23 +0300

    added serializers for orders

---
+++ code/foodcartapp/serializers.py
@@ -0,0 +1,32 @@
+from phonenumber_field.serializerfields import PhoneNumberField
+from rest_framework.serializers import CharField, ModelSerializer
+
+from .models import Order, OrderProduct
+
+
+class OrderProductSerializer(ModelSerializer):
+
+    class Meta:
+        model = OrderProduct
+        fields = ['product', 'quantity']
+
+
+class OrderSerializer(ModelSerializer):
+
+    firstname = CharField(source='first_name', max_length=50)
+    lastname = CharField(source='last_name', max_length=50)
+    phonenumber = PhoneNumberField(source='phone')
+    products = OrderProductSerializer(many=True)
+
+    class Meta:
+        model = Order
+        fields = [
+            'firstname', 'lastname', 'address', 'phonenumber', 'products',
+        ]
+
+    def create(self, validated_data):
+        products = validated_data.pop('products')
+        order = Order.objects.create(**validated_data)
+        for product in products:
+            OrderProduct.objects.create(order=order, **product)
+        return order
