0a8997fa308a56d8fa9a1821cde4cd78e14d0b2b
Oleg Smedyuk <conformist.mw@gmail.com>
2021-11-01 18:56:07 +0200

    added orders to the manager page

--- code/restaurateur/views.py
+++ code/restaurateur/views.py
@@ -6,7 +6,7 @@ from django.shortcuts import redirect, render
 from django.urls import reverse_lazy
 from django.views import View
 
-from foodcartapp.models import Product, Restaurant
+from foodcartapp.models import Order, Product, Restaurant
 
 
 class Login(forms.Form):
@@ -97,6 +97,7 @@ def view_restaurants(request):
 
 @user_passes_test(is_manager, login_url='restaurateur:login')
 def view_orders(request):
+    orders = Order.objects.all()
     return render(request, template_name='order_items.html', context={
-        # TODO заглушка для нереализованного функционала
+        'order_items': orders,
     })
