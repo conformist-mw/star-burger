49086cf019fda8c68eef2c90d88fe1a9ff948ddf
Oleg Smedyuk <conformist.mw@gmail.com>
2021-11-01 09:40:29 +0200

    test order without required fields or null values

--- code/conftest.py
+++ code/conftest.py
@@ -1,11 +1,27 @@
+from functools import partial
+
 import pytest
 from django.contrib.auth.models import User
+from django.core.management import call_command
 from rest_framework.test import APIClient
+
+from foodcartapp.models import Product
+
+
+@pytest.fixture(scope='session')
+def django_db_setup(django_db_setup, django_db_blocker):
+    with django_db_blocker.unblock():
+        call_command('loaddata', 'burgers.json')
 
 
 @pytest.fixture()
 def user(db):
-    return User.objects.create_user('user')
+    return User.objects.first()
+
+
+@pytest.fixture()
+def product(db):
+    return Product.objects.first()
 
 
 @pytest.fixture()
@@ -17,14 +33,21 @@ def client():
 def auth_client(user):
     client = APIClient()
     client.force_login(user=user)
+    client.post = partial(client.post, format='json')
     return client
 
 
 @pytest.fixture()
-def order():
+def order() -> dict:
     return {
         'firstname': 'John',
         'lastname': 'Doe',
         'phonenumber': '+380561000000',
         'address': 'New York',
     }
+
+
+@pytest.fixture()
+def order_with_products(order: dict, product) -> dict:
+    order['products'] = [{'product': product.id, 'quantity': 1}]
+    return order
--- code/foodcartapp/tests/test_create_order_api.py
+++ code/foodcartapp/tests/test_create_order_api.py
@@ -1,27 +1,57 @@
 import pytest
+from rest_framework import status
 
 API_ENDPOINT = '/api/order/'
 
 
 def test_auth_required(client):
     response = client.post(API_ENDPOINT)
-    assert response.status_code == 403
+    assert response.status_code == status.HTTP_403_FORBIDDEN
 
 
 def test_get_not_allowed(auth_client):
     response = auth_client.get(API_ENDPOINT)
-    assert response.status_code == 405
+    assert response.status_code == status.HTTP_405_METHOD_NOT_ALLOWED
 
 
 def test_empty_body(auth_client):
     response = auth_client.post(API_ENDPOINT)
-    assert response.status_code == 400
+    assert response.status_code == status.HTTP_400_BAD_REQUEST
 
 
 @pytest.mark.parametrize('products', [None, 'string', []])
 def test_order_with_wrong_products(auth_client, order, products):
     order['products'] = products
-    response = auth_client.post(API_ENDPOINT, data=order, format='json')
-    assert response.status_code == 400
+    response = auth_client.post(API_ENDPOINT, data=order)
+    assert response.status_code == status.HTTP_400_BAD_REQUEST
     assert len(response.data) == 1
     assert 'products' in response.data
+
+
+def test_order_created(auth_client, order_with_products):
+    response = auth_client.post(API_ENDPOINT, data=order_with_products)
+    assert response.status_code == status.HTTP_201_CREATED
+
+
+@pytest.mark.parametrize('missing_fields', [
+    ['firstname'],
+    ['phonenumber', 'lastname'],
+    ['firstname', 'lastname', 'phonenumber', 'address'],
+])
+def test_order_with_missing_fields(
+    auth_client, order_with_products, missing_fields,
+):
+    order = {
+        key: value for key, value in order_with_products.items()
+        if key not in missing_fields
+    }
+    response = auth_client.post(API_ENDPOINT, data=order)
+    assert response.status_code == status.HTTP_400_BAD_REQUEST
+    assert set(response.data) == set(missing_fields)
+
+
+def test_order_with_null_values(auth_client, order_with_products):
+    order = {key: None for key, _ in order_with_products.items()}
+    response = auth_client.post(API_ENDPOINT, data=order)
+    assert response.status_code == status.HTTP_400_BAD_REQUEST
+    assert set(order) == set(response.data)
