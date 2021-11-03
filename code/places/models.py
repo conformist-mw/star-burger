1516939d7928586c3bdf69ec8ac0209cb6c82a3b
Oleg Smedyuk <conformist.mw@gmail.com>
2021-11-03 23:14:49 +0200

    added method to get coordinates

--- code/places/models.py
+++ code/places/models.py
@@ -1,4 +1,7 @@
+import requests
+
 from django.db import models
+from django.conf import settings
 
 
 class Place(models.Model):
@@ -15,3 +18,25 @@ class Place(models.Model):
 
     def __str__(self):
         return f'{self.address} {self.lat} {self.lon}'
+
+    def fetch_coordinates(self):
+        base_url = 'https://geocode-maps.yandex.ru/1.x'
+        return requests.get(base_url, params={
+            'geocode': self.address,
+            'apikey': settings.YANDEX_GEO_API_KEY,
+            'format': 'json',
+        })
+
+    def set_coordinates(self):
+        response = self.fetch_coordinates()
+        if not response:
+            return
+        found_places = response.json()['response']['GeoObjectCollection']['featureMember']  # noqa: E501
+
+        if not found_places:
+            return
+
+        most_relevant = found_places[0]
+
+        self.lon, self.lat = most_relevant['GeoObject']['Point']['pos'].split(' ')  # noqa: E501
+        self.save(update_fields=['lon', 'lat'])
